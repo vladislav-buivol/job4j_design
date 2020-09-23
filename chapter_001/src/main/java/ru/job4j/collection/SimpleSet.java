f3bcc622299d05d2c82c320a5cfea0af7e5bde3b
vlad <vladislav.96@mail.ru>
2020-09-23 19:30:06 +0300

    Simple set refactoring

--- chapter_001/src/main/java/ru/job4j/collection/SimpleSet.java
+++ chapter_001/src/main/java/ru/job4j/collection/SimpleSet.java
@@ -1,59 +1,31 @@
 package ru.job4j.collection;
 
-import java.util.ConcurrentModificationException;
 import java.util.Iterator;
 
 public class SimpleSet<E> implements Iterable<E> {
-    private Object[] container;
+    private SimpleArray<E> container;
     private int containerInitSize = 10;
-    private int size = 0;
     private int modCount = 0;
 
     public SimpleSet() {
-        this.container = new Object[containerInitSize];
+        this.container = new SimpleArray<E>();
     }
 
     public void add(E e) {
-        doubleContainerSize();
         if (contains(e)) {
             return;
         }
-        modCount++;
-        container[size++] = e;
+        container.add(e);
     }
 
     @Override
     public Iterator<E> iterator() {
-        int expectedModCount = modCount;
-        return new Iterator<E>() {
-            private int point = 0;
-
-            @Override
-            public boolean hasNext() {
-                if (modCount != expectedModCount) {
-                    throw new ConcurrentModificationException();
-                }
-                return point < size;
-            }
-
-            @Override
-            public E next() {
-                return (E) container[point++];
-            }
-        };
-    }
-
-    private void doubleContainerSize() {
-        if (size >= container.length) {
-            Object[] biggerContainer = new Object[container.length * 2];
-            System.arraycopy(container, 0, biggerContainer, 0, size);
-            container = biggerContainer;
-        }
+        return container.iterator();
     }
 
     private boolean contains(E e) {
-        for (int i = 0; i < size; i++) {
-            if (e.equals(container[i])) {
+        for (int i = 0; i < container.size(); i++) {
+            if (e.equals(container.get(i))) {
                 return true;
             }
         }
@@ -61,6 +33,6 @@ public class SimpleSet<E> implements Iterable<E> {
     }
 
     public int getSize() {
-        return size;
+        return container.size();
     }
 }
