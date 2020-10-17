de704d9e5be3f30b6674e26a565ed0ba0aa4591a
vlad <vladislav.96@mail.ru>
2020-10-17 16:12:28 +0300

    AnalyzeTest refactoring

--- chapter_001/src/test/java/ru/job4j/io/AnalyzeTest.java
+++ chapter_001/src/test/java/ru/job4j/io/AnalyzeTest.java
@@ -1,26 +1,58 @@
 package ru.job4j.io;
 
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.lessThan;
 
 public class AnalyzeTest {
-    Analyze analyze;
-    ArrayList<Analyze.Entry> expected;
-    String source = "./data/testData/server.log";
-    String target = "./data/testData/unavailable.csv";
+
+    @Rule
+    public TemporaryFolder folder = new TemporaryFolder();
+    private Analyze analyze;
+    private ArrayList<Analyze.Entry> expected;
+    private File source;
+    private File target;
+    private final String serverLogData = "200 10:56:01\n" +
+            "200 10:57:01\n" +
+            "400 10:58:01\n" +
+            "200 10:59:01\n" +
+            "500 11:01:02\n" +
+            "200 11:02:02\n" +
+            "200 11:02:02\n" +
+            "300 11:02:02\n" +
+            "200 11:02:04\n" +
+            "500 11:01:05\n" +
+            "500 11:01:06\n" +
+            "200 11:02:07\n" +
+            "400 10:58:08\n" +
+            "400 10:58:09\n" +
+            "200 11:02:10\n" +
+            "500 11:01:11\n" +
+            "400 10:58:12\n" +
+            "400 10:58:12\n" +
+            "400 10:58:12\n" +
+            "400 10:58:12\n" +
+            "500 11:01:11\n" +
+            "500 11:01:11\n" +
+            "500 11:01:11\n" +
+            "500 11:01:11\n" +
+            "200 11:02:12";
+
 
     @Before
-    public void init() {
+    public void init() throws IOException {
+        source = folder.newFile("server.log");
+        target = folder.newFile("unavailable.csv");
+        writeData(serverLogData,source);
         analyze = new Analyze();
         expected = new ArrayList<>();
         expected.add(new Analyze.Entry("10:58:01", "10:59:01"));
@@ -32,26 +64,38 @@ public class AnalyzeTest {
 
     @Test
     public void analyzeLogTest() {
-        ArrayList<Analyze.Entry> actual = analyze.analyzeLog("./data/testData/server.log");
+        ArrayList<Analyze.Entry> actual = analyze.analyzeLog(source.getAbsolutePath());
         assertThat(expected, equalTo(actual));
     }
 
     @Test
     public void fileContentTest() {
-        analyze.unavailable(source, target);
-        try (BufferedReader in = new BufferedReader(new FileReader(target))) {
-            long nrOfLines = Files.lines(Paths.get(target)).count();
+        String pathToSource = source.getAbsolutePath();
+        String pathToTarget = target.getAbsolutePath();
+        analyze.unavailable(pathToSource, pathToTarget);
+        System.out.println(pathToSource);
+        try (BufferedReader in = new BufferedReader(new FileReader(pathToTarget))) {
+            long nrOfLines = Files.lines(Paths.get(pathToTarget)).count();
             assertThat(5L, equalTo(nrOfLines));
             int index = 0;
             String line;
-            while ((line = in.readLine()) != null){
+            while ((line = in.readLine()) != null) {
                 String[] parts = line.split(";");
                 String startTime = parts[0];
                 String endTime = parts[1];
-                Analyze.Entry currentLine = new Analyze.Entry(startTime,endTime);
-                assertThat(expected.get(index++),equalTo(currentLine));
+                Analyze.Entry currentLine = new Analyze.Entry(startTime, endTime);
+                assertThat(expected.get(index++), equalTo(currentLine));
             }
         } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void writeData(String data, File target){
+        try (PrintWriter out = new PrintWriter(target)){
+            data = data.replace("\n",System.lineSeparator());
+            out.write(data);
+        }catch (Exception e){
             e.printStackTrace();
         }
     }
