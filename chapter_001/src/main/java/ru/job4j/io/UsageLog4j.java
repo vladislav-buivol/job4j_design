261ca22b186124dee35eae874e64d1e8959a5a21
vlad <vladislav.96@mail.ru>
2020-10-31 17:27:59 +0200

    Slf4j variable output

--- chapter_001/src/main/java/ru/job4j/io/UsageLog4j.java
+++ chapter_001/src/main/java/ru/job4j/io/UsageLog4j.java
@@ -8,10 +8,18 @@ public class UsageLog4j {
     private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
 
     public static void main(String[] args) {
-        LOG.trace("trace message");
-        LOG.debug("debug message");
-        LOG.info("info message");
-        LOG.warn("warn message");
-        LOG.error("error message");
+        byte importantData = (byte) 109;
+        long id = 123143253425234L;
+        String name = "Petr Arsentev";
+        char gender = 'm';
+        int age = 33;
+        double salary = 4312523.43d;
+        float weight = 77.7f;
+        short favoriteNumber = 7;
+        LOG.debug("User info name : {}, age : {}, ID: {}", name, age, id);
+        LOG.info("User info name : {}, gender: {}", name, gender);
+        LOG.error("Important data is incorrect. ID: {}, important data {}", id, importantData);
+        LOG.warn("Data is incorrect. ID:{}, name:{}, salary:{}, weight:{}, favorite number:{}", id, name, salary, weight, favoriteNumber);
     }
+
 }
