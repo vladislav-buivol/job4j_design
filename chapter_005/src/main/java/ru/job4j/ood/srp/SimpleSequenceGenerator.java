b5d376b6913b1d7cdcecd4a3cae58a6e4b499ed2
vlad <vladislav.96@mail.ru>
2020-12-27 16:31:16 +0200

    Single Responsibility Principle

---
+++ chapter_005/src/main/java/ru/job4j/ood/srp/SimpleSequenceGenerator.java
@@ -0,0 +1,21 @@
+package ru.job4j.ood.srp;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class SimpleSequenceGenerator implements SequenceGenerator<Integer> {
+
+    private final NumberGenerator<Integer> numberGenerator;
+
+    public SimpleSequenceGenerator(NumberGenerator<Integer> numberGenerator) {
+        this.numberGenerator = numberGenerator;
+    }
+
+    @Override
+    public List<Integer> generate(int size) {
+        return IntStream.range(0, size)
+                .map(i -> numberGenerator.generate()).boxed()
+                .collect(Collectors.toList());
+    }
+}
