219bbad0b6d1a23ad015c6a327d06e96cfaeda75
vlad <vladislav.96@mail.ru>
2021-02-22 21:11:55 +0200

    XML searilization

--- chapter_001/src/main/java/ru/job4j/io/serialization/xml/CarXml.java
+++ chapter_001/src/main/java/ru/job4j/io/serialization/xml/CarXml.java
@@ -1,38 +1,93 @@
 package ru.job4j.io.serialization.xml;
 
-import ru.job4j.io.Person;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+import javax.xml.bind.annotation.*;
+import java.io.StringReader;
+import java.io.StringWriter;
+import java.util.Arrays;
 
+@XmlRootElement(name = "car")
+@XmlAccessorType(XmlAccessType.FIELD)
 public class CarXml {
-    private final boolean registered;
-    private final PersonXML owner;
-    private final PersonXML[] users;
-    private final String carNumber;
-    private final int registrationYear;
-    final String carXML = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
-            + " <car registered=\"true\" owner=\"Bob\" carNumber=\"123ASD\" registrationYear=\"2020\">"
-            + "    <users>"
-            + "        <person sex=\"false\" age=\"30\">"
-            + "            <contact phone=\"11-111\"/>"
-            + "            <statuses>"
-            + "                <status>Worker</status>"
-            + "                <status>Married</status>"
-            + "            </statuses>"
-            + "        </person>"
-            + "        <person sex=\"false\" age=\"44\">"
-            + "            <contact phone=\"22-222\"/>"
-            + "            <statuses>"
-            + "                <status>Worker</status>"
-            + "                <status>Married</status>"
-            + "            </statuses>"
-            + "        </person>"
-            + "    </users>"
-            + " </car>";
+    @XmlAttribute
+    private boolean registered;
 
-    public CarXml(boolean registered, PersonXML owner, PersonXML[] users, String carNumber, int registrationYear) {
+
+    private Person owner;
+
+    @XmlElementWrapper(name = "users")
+    @XmlElement(name = "user")
+    private Person[] users;
+
+    @XmlAttribute
+    private String carNumber;
+
+    @XmlAttribute
+    private int registrationYear;
+
+
+    public static void main(String[] args) throws Exception {
+        Person person = new Person(false, 30, new Contact("11-111"), "Worker", "Married");
+        Person person2 = new Person(true, 44, new Contact("22-22"), "Worker", "Married");
+        CarXml car = new CarXml(true, person, new Person[]{person}, "123asd", 2020);
+        JAXBContext context = JAXBContext.newInstance(CarXml.class);
+        Marshaller marshaller = context.createMarshaller();
+        marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+        String xml = "";
+        try (StringWriter writer = new StringWriter()) {
+            marshaller.marshal(car, writer);
+            xml = writer.getBuffer().toString();
+            System.out.println(xml);
+        }
+
+        String carXML = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
+                + " <car registered=\"true\" owner=\"Bob\" carNumber=\"123ASD\" registrationYear=\"2020\">"
+                + "    <users>"
+                + "        <person sex=\"false\" age=\"30\">"
+                + "            <contact phone=\"11-111\"/>"
+                + "            <statuses>"
+                + "                <status>Worker</status>"
+                + "                <status>Married</status>"
+                + "            </statuses>"
+                + "        </person>"
+                + "        <person sex=\"false\" age=\"44\">"
+                + "            <contact phone=\"22-222\"/>"
+                + "            <statuses>"
+                + "                <status>Worker</status>"
+                + "                <status>Married</status>"
+                + "            </statuses>"
+                + "        </person>"
+                + "    </users>"
+                + " </car>";
+        Unmarshaller unmarshaller = context.createUnmarshaller();
+        try (StringReader reader = new StringReader(carXML)) {
+            CarXml carFromString = (CarXml) unmarshaller.unmarshal(reader);
+            System.out.println(carFromString);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "CarXml{"
+                + "registered=" + registered
+                + ", owner=" + owner
+                + ", users=" + Arrays.toString(users)
+                + ", carNumber='" + carNumber + '\''
+                + ", registrationYear=" + registrationYear
+                + '}';
+    }
+
+    public CarXml(boolean registered, Person owner, Person[] users, String carNumber, int registrationYear) {
         this.registered = registered;
         this.owner = owner;
         this.users = users;
         this.carNumber = carNumber;
         this.registrationYear = registrationYear;
     }
+
+    public CarXml() {
+    }
+
 }
